b83072b1272fe5c0c9d0920505f42c39ec8fa22c
Arsen <arsen.khadikov@gmail.com>
2016-03-23 00:16:53 +0200

    mafucka

--- 1.py
+++ 1.py
@@ -5,15 +5,26 @@ from flask_bootstrap import Bootstrap
 from flask import Flask, render_template
 from database_setup import Base, User
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
 
 
 app = Flask(__name__)
 engine = create_engine('sqlite:///user.db')
 Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
 @app.route('/')
 def hello_world():
-    user = User(name='Arsen', email='arsen.khadikov@gmail.com')
+    session.add(User(name='Arsen', email='arsen.khadikov@gmail.com'))
+    session.commit()
+    user = session.query(User).first()
     return "%s %s"%(user.name, user.email)
+
+
 
 if __name__ == '__main__':
     Bootstrap(app.run())
