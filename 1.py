441afd4f7291c9687d517436bab8369c1ea347e0
Arsen <arsen.khadikov@gmail.com>
2016-03-24 14:24:24 +0200

    reg design

--- 1.py
+++ 1.py
@@ -6,6 +6,7 @@ from flask import Flask, render_template, request, redirect, jsonify, url_for
 from database_setup import Base, User
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import re
 
 
 
@@ -13,8 +14,35 @@ from sqlalchemy.orm import sessionmaker
 app = Flask(__name__)
 engine = create_engine('sqlite:///user.db')
 Base.metadata.bind = engine
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
+DBsession = sessionmaker(bind=engine)
+db_session = DBsession()
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PASS_RE = re.compile(r"^.{3,20}$")
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
+
+
+def valid_username(username):
+    if USER_RE.match(username):
+        return True
+    return False
+
+
+def valid_password(password):
+    if PASS_RE.match(password):
+        return True
+    return False
+
+
+def valid_email(email):
+    if EMAIL_RE.match(email):
+        return True
+    return False
+
+
+def valid_verify(password, verify):
+    if password == verify:
+        return True
+    return False
 
 
 @app.route('/registration', methods=['GET', 'POST'])
@@ -22,36 +50,41 @@ def registration():
     if request.method == 'GET':
         return render_template('registration.html')
     else:
-        newUser = User(nickname=request.form['nickname'], email=request.form['email'], password=request.form['password'])
-        session.add(newUser)
-        session.commit()
-        return redirect(url_for('hello_world'))
+        nickname = request.form['nickname'] 
+        email = request.form['email']
+        password = request.form['password']
+        verify_password = request.form['verify_password']
+        if valid_username(nickname) and valid_email(email) and valid_password(password) and valid_verify(password, verify_password):
+            newUser = User(nickname=nickname, email=email, password=password)
+            db_session.add(newUser)
+            db_session.commit()
+            return redirect(url_for('hello_world'))
+        else:
+            return redirect(url_for('registration'))
 
-#@app.route('login', methods=[])
-#def login():
 
 @app.route('/user/JSON')
 def restaurantsJSON():
-    users = session.query(User).all()
+    users = db_session.query(User).all()
     return jsonify(users=[u.serialize for u in users])
 
 
+@app.route('/<int:user_id>')
 @app.route('/user/<int:user_id>')
 def show_user(user_id):
-    user = session.query(User).filter_by(id=user_id).one()
+    user = db_session.query(User).filter_by(id=user_id).one()
     return render_template('show_user.html', user=user)
 
 
 @app.route('/user')
 @app.route('/')
 def hello_world():
-    #session.add(User(nickname='Arsen', password='123', email='arsen.khadikov@gmail.com'))
-    #session.add(User(nickname='Vitya', password='123', email='vitya.sobol@gmail.com'))
-    #session.add(User(nickname='Vlad', password='123', email='vlad.fedchenko@gmail.com'))
-    #session.commit()
-    users = session.query(User).all()
+    #db_session.add(User(nickname='Arsen', password='123', email='arsen.khadikov@gmail.com'))
+    #db_session.add(User(nickname='Vitya', password='123', email='vitya.sobol@gmail.com'))
+    #db_session.add(User(nickname='Vlad', password='123', email='vlad.fedchenko@gmail.com'))
+    #db_session.commit()
+    users = db_session.query(User).all()
     return render_template('hello_world.html', users=users)
-    # return ""
 
 
 
